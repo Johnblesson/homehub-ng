0cd6aeb84a11504b425b5db6ae99222561320b92
Johnblesson <johnblessonrowe@gmail.com>
2024-08-06 03:30:22 +0000

    update

--- server/render/sitemap.js
+++ server/render/sitemap.js
@@ -5,19 +5,26 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
-
 <url>
   <loc>https://www.homehubng.com/</loc>
   <lastmod>2024-08-03T13:35:58+00:00</lastmod>
   <priority>1.00</priority>
 </url>
 <url>
+  <loc>https://www.homehubng.com/login</loc>
+  <lastmod>2024-08-03T13:35:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
   <loc>https://www.homehubng.com/signup</loc>
   <lastmod>2024-08-03T13:35:58+00:00</lastmod>
   <priority>0.64</priority>
 </url>
-
+<url>
+  <loc>https://www.homehubng.com/privacy-policy</loc>
+  <lastmod>2024-08-03T13:35:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
 
 </urlset>`;
 
